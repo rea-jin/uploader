906a9d8af5de2e0b91813d35961eefe8b6955cbd
rea-jin <56153015+rea-jin@users.noreply.github.com>
2019-12-24 13:19:41 +0900

    commit

--- auth.php
+++ auth.php
@@ -9,11 +9,11 @@ if( !empty($_SESSION['login_date']) ){
   // 現在日時が最終ログイン日時＋有効期限を超えていた場合
   if( ($_SESSION['login_date'] + $_SESSION['login_limit']) < time()){
     debug('ログイン有効期限オーバーです。');
-
     // セッションを削除（ログアウトする）
     session_destroy();
     // ログインページへ
     header("Location:login-h.php");
+    
   }else{
     debug('ログイン有効期限以内です。');
     //最終ログイン日時を現在日時に更新
